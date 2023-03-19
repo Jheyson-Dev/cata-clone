071c934a9016e25103917d6404c92675498bc689
Jheyson-Dev <Jheyson-Dev@users.noreply.github.com>
2023-03-19 10:29:25 -0500

    configuracion de los colores de la pagina

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue-primary': '#3b71ad',
+        'yellow-primary': '#ffbd59',
+        'text-primary': '#53565a',
+        'button-primary': '#66b1ff',
+        'button-secondary': '#409eff',
+        'banner-bg': '#003366',
+      },
+    },
   },
   plugins: [],
 };
