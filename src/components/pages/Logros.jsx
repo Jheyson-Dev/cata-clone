116705a2b4cd5d83b2fbf28d71e26ba3ddc3f55e
Jheyson-Dev <Jheyson-Dev@users.noreply.github.com>
2023-03-31 17:41:47 -0500

    creacion de la pagina Logros

--- src/components/pages/Logros.jsx
+++ src/components/pages/Logros.jsx
@@ -1,7 +1,90 @@
+import { Banner } from '../sections/Banner';
+import logro1 from '../../assets/img/logros/Logro_1.webp';
+import logro2 from '../../assets/img/logros/Logro_2.webp';
+import logro3 from '../../assets/img/logros/Logro_3.webp';
+import logro4 from '../../assets/img/logros/Logro_4.webp';
+import logro5 from '../../assets/img/logros/Logro_5.webp';
+import logro6 from '../../assets/img/logros/Logro_6.webp';
+import logro7 from '../../assets/img/logros/Logro_7.webp';
+import logro8 from '../../assets/img/logros/Logro_8.webp';
+import logro9 from '../../assets/img/logros/Logro_9.webp';
+import logro10 from '../../assets/img/logros/Logro_10.webp';
+import { useState } from 'react';
+import { Image } from 'antd';
+
 export const Logros = () => {
+  const [logro, setLogro] = useState([
+    {
+      id: 1,
+      img: logro1,
+    },
+    {
+      id: 2,
+      img: logro2,
+    },
+    {
+      id: 3,
+      img: logro3,
+    },
+    {
+      id: 4,
+      img: logro4,
+    },
+    {
+      id: 5,
+      img: logro5,
+    },
+    {
+      id: 6,
+      img: logro6,
+    },
+    {
+      id: 7,
+      img: logro7,
+    },
+    {
+      id: 8,
+      img: logro8,
+    },
+    {
+      id: 9,
+      img: logro9,
+    },
+    {
+      id: 10,
+      img: logro10,
+    },
+  ]);
+
   return (
     <div>
-      <h2>Logros</h2>
+      <Banner text={'Logros'} />
+      <div className='p-10'>
+        <div className='grid gap-5'>
+          <div>
+            <h2 className='text-3xl text-yellow-primary font-semibold text-center'>
+              Entre los primeros en diversos concursos
+            </h2>
+          </div>
+          <span className='text-gray-400 text-sm font-semibold'>
+            Representamos a la cuidad de Juliaca en cualquier evento...
+          </span>
+          <div className='grid grid-cols-2 gap-5'>
+            {logro.map(({ id, img }) => {
+              return (
+                <div key={id}>
+                  <Image
+                    src={img}
+                    alt=''
+                    rootClassName='bg-black'
+                    className='rounded-lg shadow-lg'
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
